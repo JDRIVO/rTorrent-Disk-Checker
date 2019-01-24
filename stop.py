852848de87094f471bef02f12eabacbfc590a79a
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-01-24 23:25:02 +1100

    optimize

--- stop.py
+++ stop.py
@@ -1,17 +1,9 @@
 import sys
 from xmlrpc import xmlrpc
 
-decimals = sys.argv[1].split(' ')
-hexadecimal = []
-
-for decimal in decimals:
-        hexadecimal.append(format(int(decimal), '02x'))
-
-torrent_hash = ''.join(hexadecimal).upper()
-
 while True:
         try:
-                xmlrpc('d.stop', tuple([torrent_hash]))
+                xmlrpc('d.stop', tuple([sys.argv[1]]))
                 break
         except:
                 pass
