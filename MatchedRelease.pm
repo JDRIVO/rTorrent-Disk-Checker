5eee3a1dc039236cde5227a5d01af198bdaef7a2
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-01-24 23:23:35 +1100

    optimize

--- MatchedRelease.pm
+++ MatchedRelease.pm
@@ -1,5 +1,5 @@
-        my @decimals = unpack 'U*', $self->{info_hash};
-        my @script = split $/, `python2 /path/to/autodlcheck.py "$self->{ti}{torrentName}" "$self->{uploadMethod}{rtLabel}" "$self->{ti}{torrentSizeInBytes}" "@decimals"`;
+        my $torrentHash = dataToHex($self->{info_hash});
+        my @script = split $/, `python2 /path/to/autodlcheck.py "$self->{ti}{torrentName}" "$self->{uploadMethod}{rtLabel}" "$self->{ti}{torrentSizeInBytes}" "$torrentHash"`;
 
         if ($script[0] eq "exit") {
                 return;
