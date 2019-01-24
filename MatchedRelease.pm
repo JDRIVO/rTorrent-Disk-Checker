f7bfa97183cf99deb8855c7821df9e2e5a5f40c8
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-01-24 18:40:10 +1100

    Update MatchedRelease.pm

--- MatchedRelease.pm
+++ MatchedRelease.pm
@@ -1,5 +1,5 @@
         my @decimals = unpack 'U*', $self->{info_hash};
-        my @script = split $/, `python /path/to/autodlcheck.py '$self->{ti}{torrentName}' '$self->{uploadMethod}{rtLabel}' '$self->{ti}{torrentSizeInBytes}' '@decimals'`;
+        my @script = split $/, `python2 /path/to/autodlcheck.py '$self->{ti}{torrentName}' '$self->{uploadMethod}{rtLabel}' '$self->{ti}{torrentSizeInBytes}' '@decimals'`;
 
         if ($script[0] eq "exit") {
                 return;
