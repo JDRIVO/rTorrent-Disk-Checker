b129db3e743622c4f10eadae350a7480cd89b4e9
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-02-09 07:02:33 +1100

    Update MatchedRelease.pm

--- Deprecated/MatchedRelease.pm
+++ Deprecated/MatchedRelease.pm
@@ -1,4 +1,5 @@
-        my @script = split $/, `python /path/to/autodlcheck.py '$self->{ti}{torrentName}' '$self->{uploadMethod}{rtLabel}' '$self->{ti}{torrentSizeInBytes}'`;
+        my $torrentHash = dataToHex($self->{info_hash});
+        my @script = split $/, `python2 /path/to/autodlcheck.py "$self->{ti}{torrentName}" "$self->{uploadMethod}{rtLabel}" "$self->{ti}{torrentSizeInBytes}" "$torrentHash"`;
 
         if ($script[0] eq "exit") {
                 return;
