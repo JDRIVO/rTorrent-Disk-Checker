af66f5d78a13b789e1e5986e23313a1f1b9b732b
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-02-09 14:12:02 +1100

    Delete MatchedRelease.pm

--- Deprecated/MatchedRelease.pm
+++
@@ -1,7 +0,0 @@
-        my $torrentHash = dataToHex($self->{info_hash});
-        my @script = split $/, `python2 /path/to/autodlcheck.py "$self->{ti}{torrentName}" "$self->{uploadMethod}{rtLabel}" "$self->{ti}{torrentSizeInBytes}" "$torrentHash"`;
-
-        if ($script[0] eq "exit") {
-                return;
-        }
-
