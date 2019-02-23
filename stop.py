e71150b40543862626ed9b66f5b5327c708ed83e
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-02-24 10:36:19 +1100

    Delete stop.py

--- stop.py
+++
@@ -1,9 +0,0 @@
-import sys
-from remotecall import xmlrpc
-
-while True:
-        try:
-                xmlrpc('d.stop', tuple([sys.argv[1]]))
-                break
-        except:
-                pass
