0f917a924985484f4676c8e0a8f3c898b1480750
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-03-29 16:05:38 +1100

    Update remover.py

--- remover.py
+++ remover.py
@@ -1,30 +1,74 @@
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys, os, time, shutil
 from remotecall import xmlrpc
 
-torrent_hash = sys.argv[1]
-torrent_path = sys.argv[2]
+queue = sys.argv[1]
+queue_position = sys.argv[2]
+torrent_hash = sys.argv[3]
+torrent_path = sys.argv[4]
+
+def remover():
+        t_hash = tuple([torrent_hash])
+        xmlrpc('d.tracker_announce', t_hash)
+        xmlrpc('d.open', t_hash)
+        files = xmlrpc('f.multicall', (torrent_hash, '', 'f.frozen_path='))
+        xmlrpc('d.erase', t_hash)
+
+        if len(files) <= 1:
+                os.remove(files[0][0])
+        else:
+                [os.remove(file[0]) for file in files]
+
+                try:
+                        os.rmdir(torrent_path)
+                except:
+
+                        for root, directories, files in os.walk(torrent_path, topdown=False):
+
+                                try:
+                                        os.rmdir(root)
+                                except:
+                                        pass
 
 
-t_hash = tuple([torrent_hash])
-xmlrpc('d.tracker_announce', t_hash)
-xmlrpc('d.open', t_hash)
-files = xmlrpc('f.multicall', (torrent_hash, '', 'f.frozen_path='))
-xmlrpc('d.erase', t_hash)
+in_front = str(int(queue_position) - 1)
+copy = queue + 'c'
 
-if len(files) <= 1:
-        os.remove(files[0][0])
+with open(queue, 'a+') as txt:
+        txt.write(queue_position + '\n')
+
+time.sleep(0.01)
+
+with open(queue, 'r') as txt:
+        queued = txt.read()
+
+if in_front not in queued:
+        remover()
 else:
-        [os.remove(file[0]) for file in files]
 
-        try:
-                os.rmdir(torrent_path)
-        except:
+        while True:
 
-                for root, directories, files in os.walk(torrent_path, topdown=False):
+                with open(queue, 'r') as txt:
+                        queued = txt.read()
 
-                        try:
-                                os.rmdir(root)
-                        except:
-                                pass
+                if in_front not in queued:
+                        remover()
+                        break
+
+                time.sleep(0.01)
+
+with open(copy, 'w+') as txt:
+
+        for number in queued.strip():
+
+                if number != queue_position:
+                        txt.write(number + '\n')
+
+shutil.move(copy, queue)
+time.sleep(180)
+
+try:
+        os.remove(queue)
+except:
+        pass
