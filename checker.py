e69fff26410da7fddc14fe6eca18dd96143c2610
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-03-29 12:11:41 +1100

    Update checker.py

--- checker.py
+++ checker.py
@@ -55,7 +55,6 @@ if cfg.enable_disk_check:
         try:
                 last_hash = open(last_dl).readline()
                 downloading = xmlrpc('d.left.bytes', tuple([last_hash]))
-                
         except:
                 downloading = 0
                 
