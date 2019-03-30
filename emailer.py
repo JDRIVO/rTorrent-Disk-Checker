ebbe18700f54bac15d58c601d0817a87be781a27
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-03-30 19:44:01 +1100

    Create emailer.py

---
+++ emailer.py
@@ -0,0 +1,26 @@
+import os, sys, time, smtplib, config as cfg
+
+lock = os.path.dirname(sys.argv[0]) + '/email.txt'
+cfg.interval *= 60
+
+if os.path.isfile(lock):
+        quit()
+else:
+
+        with open(lock, 'w+') as txt:
+                txt.write('1')
+
+try:
+        server = smtplib.SMTP_SSL(cfg.smtp_server, cfg.port)
+        server.login(cfg.account, cfg.password)
+except:
+        server = smtplib.SMTP(cfg.smtp_server, cfg.port)
+        server.starttls()
+        server.login(cfg.account, cfg.password)
+
+message = 'Subject: {}\n\n{}'.format(cfg.subject, cfg.body)
+server.sendmail(cfg.account, cfg.receiver, message)
+server.quit()
+
+time.sleep(cfg.interval)
+os.remove(lock)
