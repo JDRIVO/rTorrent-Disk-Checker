e426e67b3cd02c3edb83d75ab907c07f7c30a9d9
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-03-30 23:01:57 +1100

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -10,10 +10,13 @@ else:
                 txt.write('1')
 
 try:
-        server = smtplib.SMTP_SSL(cfg.smtp_server, cfg.port, timeout=10)
-        server.login(cfg.account, cfg.password)
+        try:
+                server = smtplib.SMTP_SSL(cfg.smtp_server, cfg.port, timeout=10)
+                server.login(cfg.account, cfg.password)
+        except:
+                server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
+                server.login(cfg.account, cfg.password)
 except:
-        server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
         server.starttls()
         server.login(cfg.account, cfg.password)
 
