3055a3cba6a485820ae4c8d9bf8a0dc4e9deb1f7
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-04 10:25:38 +1100

    Update test.py

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ if cfg.enable_disk_check:
         downloading = 0
         #downloading = sum(torrent[0] for torrent in downloading) if downloading else 0
         disk = os.statvfs('/')
-        available_space = (disk.f_bsize * disk.f_bavail - downloading) / 1073741824.0
+        available_space = (sum(disk.f_bsize * disk.f_bavail for disk in [os.statvfs(path) for path in cfg.mount_points]) - downloading) / 1073741824.0
         required_space = torrent_size - (available_space - cfg.minimum_space)
         requirements = cfg.minimum_size, cfg.minimum_age, cfg.minimum_ratio, cfg.fallback_age, cfg.fallback_ratio
         current_date = datetime.now()
