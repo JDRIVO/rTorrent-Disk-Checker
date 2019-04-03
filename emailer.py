c0b333f0ccc67763b66532483584f47e1b434358
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-03 12:16:28 +1100

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,48 +1,40 @@
 import os, sys, datetime, smtplib, config as cfg
 
-def last_email():
-        lock = os.path.dirname(sys.argv[0]) + '/email.txt'
+lock = os.path.dirname(sys.argv[0]) + '/email.txt'
 
-        if os.path.isfile(lock):
-                file_age = datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getctime(lock))
+if os.path.isfile(lock):
+        file_age = datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getctime(lock))
 
-                if file_age < datetime.timedelta(minutes=cfg.interval):
-                        quit()
+        if file_age < datetime.timedelta(minutes=cfg.interval):
+                quit()
 
-        with open(lock, 'w+') as txt:
-                txt.write('1')
+with open(lock, 'w+') as txt:
+        txt.write('1')
 
-def send_email():
-        server = False
+print('\n1st Traceback block is TLS related\n2nd Traceback block is SSL related\n3rd Traceback block is Non TLS/SSL related\n')
+server = False
+
+try:
 
         try:
-
-                try:
-                        server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
-                        server.starttls()
-                        server.login(cfg.account, cfg.password)
-                except:
-
-                        if server:
-                                server.quit()
-
-                        server = smtplib.SMTP_SSL(cfg.smtp_server, cfg.port, timeout=10)
-                        server.login(cfg.account, cfg.password)
+                server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
+                server.starttls()
+                server.login(cfg.account, cfg.password)
         except:
 
                 if server:
                         server.quit()
 
-                server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
+                server = smtplib.SMTP_SSL(cfg.smtp_server, cfg.port, timeout=10)
                 server.login(cfg.account, cfg.password)
+except:
 
-        message = 'Subject: {}\n\n{}'.format(cfg.subject, cfg.body)
-        server.sendmail(cfg.account, cfg.receiver, message)
-        server.quit()
+        if server:
+                server.quit()
 
-if __name__ == '__main__':
-        print('\n1st Traceback block is TLS related\n2nd Traceback block is SSL related\n3rd Traceback block is Non TLS/SSL related\n')
-        send_email()
-else:
-        last_email()
-        send_email()
+        server = smtplib.SMTP(cfg.smtp_server, cfg.port, timeout=10)
+        server.login(cfg.account, cfg.password)
+
+message = 'Subject: {}\n\n{}'.format(cfg.subject, cfg.body)
+server.sendmail(cfg.account, cfg.receiver, message)
+server.quit()
