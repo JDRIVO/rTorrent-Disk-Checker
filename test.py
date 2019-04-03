3f416bb931023bdbf40b6e22b26f3b83e73ce2c5
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-03 18:44:43 +1100

    Update test.py

--- test.py
+++ test.py
@@ -163,7 +163,10 @@ with open('testresult.txt', 'w+') as textfile:
                         textfile.write(result.encode('utf-8') + '\n')
 
 print('\nTA = Torrent Age  TN = Torrent Name  TL = Torrent Label  TT = Torrent Tracker\n')
-[print(result) for result in deleted]
+
+for result in deleted:
+        print(result)
+
 print('TA = Torrent Age  TN = Torrent Name  TL = Torrent Label  TT = Torrent Tracker\n')
 print('Script Executed in %s Seconds\n%s Torrent(s) Deleted Totaling %.2f GB' % (time, count, freed_space))
 print('%.2f GB Free Space Before Torrent Download\n%.2f GB Free Space After %.2f GB Torrent Download\n' % (available_space, calc, torrent_size))
