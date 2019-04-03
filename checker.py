c9cd2d0967fc5397814570d5665d9bf99023e6ec
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-04 10:27:16 +1100

    Update checker.py

--- checker.py
+++ checker.py
@@ -93,7 +93,6 @@ if cfg.enable_disk_check:
                 downloading = 0
 
         open(last_torrent, mode='w+').write(torrent_hash)
-        disk = os.statvfs('/')
         available_space = (sum(disk.f_bsize * disk.f_bavail for disk in [os.statvfs(path) for path in cfg.mount_points]) - downloading) / 1073741824.0
         required_space = torrent_size - (available_space - cfg.minimum_space)
         requirements = cfg.minimum_size, cfg.minimum_age, cfg.minimum_ratio, cfg.fallback_age, cfg.fallback_ratio
