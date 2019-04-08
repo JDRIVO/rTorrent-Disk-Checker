2da93817023a4a4cc3c4c69f73806e85bc3c512b
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-08 17:31:13 +1000

    Update cachebuilder.py

--- cachebuilder.py
+++ cachebuilder.py
@@ -9,8 +9,8 @@ mp_cache = script_path + '/mountpoints.py'
 
 def build_cache():
         completed = xmlrpc('d.multicall2', ('', 'complete', 'd.timestamp.finished=', 'd.custom1=', 't.multicall=,t.url=', 'd.ratio=', 'd.size_bytes=', 'd.name=', 'd.hash=', 'd.directory='))
-        completed.sort()
         [list.append(list[7].rsplit('/', 1)[0]) if list[5] in list[7] else list.append(list[7]) for list in completed]
+        completed = {list[6]: list for list in completed}
 
         if os.path.isfile(torrent_cache):
                 cache = open(torrent_cache, mode='r+')
