8cdb00ea37734bf57b4e2bf78d9fe7f3074c53fb
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-10 17:32:53 +1000

    Update cacher.py

--- cacher.py
+++ cacher.py
@@ -63,4 +63,5 @@ def build_cache(identity):
                 open(mp_cache, mode='w+').write('mount_points = ' + pprint.pformat(mount_points))
 
 if __name__ == "__main__":
-        build_cache('schedule')
+        import random
+        build_cache('schedule' + str(random.randint(0, 99999999999)))
