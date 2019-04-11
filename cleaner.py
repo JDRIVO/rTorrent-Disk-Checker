db5d78bb44f542c25cc0f1ba35f173326d63fb24
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2019-04-12 02:13:02 +1000

    cleanup queues on rtorrent start

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,6 @@
 import sys, os, time
 
 time.sleep(0.5)
-directory =  os.path.dirname(sys.argv[0])
+directory = os.path.dirname(sys.argv[0])
 files = os.listdir(directory)
 [os.remove(file) for file in files if file.endswith('.txt')]
