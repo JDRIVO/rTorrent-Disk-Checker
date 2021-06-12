bf612f49290d5ca147fda120a5b0e1edefbd565b
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-06-12 16:52:04 +1000

    Update remote_caller.py

--- remote_caller.py
+++ remote_caller.py
@@ -1,65 +1,63 @@
 from config import scgi
 
 try:
-        import xmlrpclib, socket
-        import urllib, urlparse
+		import xmlrpclib, socket
+		import urllib
 except:
-        import xmlrpc.client as xmlrpclib, socket
-        from urllib import parse as urllib
+		import xmlrpc.client as xmlrpclib, socket
+		from urllib import parse as urllib
 
-class SCGIRequest(object):
-        def __init__(self, url):
-                self.url = url
+class SCGIRequest:
 
-        def __send(self, scgireq):
+		def __init__(self):
+			self.url = scgi
 
-                try:
-                        host, port = self.url.split(':')
-                        addrinfo = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
-                        sock = socket.socket(*addrinfo[0][:3])
-                        sock.connect(addrinfo[0][4])
-                except:
-                        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-                        sock.connect(self.url)
+		def send(self, methodname, params):
+			"Send data over scgi to url and get response"
+			data = xmlrpclib.dumps(params, methodname)
+			scgiresp = self.__send(self.addRequiredSCGIHeaders(data) )
+			xmlresp = ''.join(scgiresp.split('\n')[4:])
+			return xmlrpclib.loads(xmlresp)[0][0]
 
-                sock.send(scgireq.encode() )
-                sfile = sock.makefile()
-                response = ''
+		def __send(self, scgireq):
 
-                while True:
-                        data = sfile.read(1024)
+			try:
+				host, port = self.url.split(':')
+				addrinfo = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+				sock = socket.socket(*addrinfo[0][:3])
+				sock.connect(addrinfo[0][4])
+			except:
+				sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+				sock.connect(self.url)
 
-                        if not data:
-                                break
+			sock.send(scgireq.encode() )
+			sfile = sock.makefile()
+			response = ''
 
-                        response += data
+			while True:
+				data = sfile.read(1024)
 
-                sock.close()
-                return urllib.unquote(response)
+				if not data:
+					break
 
-        def send(self, data):
-                "Send data over scgi to url and get response"
-                scgiresp = self.__send(self.add_required_scgi_headers(data) )
-                return ''.join(scgiresp.split('\n')[4:])
+				response += data
 
-        @staticmethod
-        def encode_netstring(string):
-                "Encode string as netstring"
-                return '%d:%s,' % (len(string), string)
+			sock.close()
+			return urllib.unquote(response)
 
-        @staticmethod
-        def make_headers(headers):
-                "Make scgi header list"
-                return '\x00'.join(['%s\x00%s' % t for t in headers]) + '\x00'
+		@staticmethod
+		def encodeNetstring(string):
+			"Encode string as netstring"
+			return '%d:%s,' % (len(string), string)
 
-        @staticmethod
-        def add_required_scgi_headers(data, headers = []):
-                "Wrap data in an scgi request,\nsee spec at: http://python.ca/scgi/protocol.txt"
-                headers = SCGIRequest.make_headers([('CONTENT_LENGTH', str(len(data) ) ), ('SCGI', '1'),] + headers)
-                enc_headers = SCGIRequest.encode_netstring(headers)
-                return enc_headers + data
+		@staticmethod
+		def makeHeaders(headers):
+			"Make scgi header list"
+			return '\x00'.join(['%s\x00%s' % t for t in headers]) + '\x00'
 
-def xmlrpc(methodname, params):
-        xmlreq = xmlrpclib.dumps(params, methodname)
-        xmlresp = SCGIRequest(scgi).send(xmlreq)
-        return xmlrpclib.loads(xmlresp)[0][0]
+		@staticmethod
+		def addRequiredSCGIHeaders(data, headers = []):
+			"Wrap data in an scgi request, see spec at: http://python.ca/scgi/protocol.txt"
+			headers = SCGIRequest.makeHeaders([('CONTENT_LENGTH', str(len(data) ) ), ('SCGI', '1'),] + headers)
+			encHeaders = SCGIRequest.encodeNetstring(headers)
+			return encHeaders + data
