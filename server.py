bddb7382ad4309fcf9c24b5578cc69693ea13290
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2021-06-27 01:13:58 +1000

    Update log path

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
+import os
 import logging
-logging.basicConfig(filename="server.log", level=logging.DEBUG, format="%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
+logging.basicConfig(filename=os.path.abspath(os.getcwd() ) + "/server.log", level=logging.DEBUG, format="%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
 
 import socket
-import os
 import sys
 import subprocess
 from threading import Thread
