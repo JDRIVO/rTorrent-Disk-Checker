588a9353d328b03f79af21a4d26f5df3eb4bbbbb
GangaBanga <41508421+GangaBanga@users.noreply.github.com>
2021-06-27 14:43:35 +1000

    Update config.py

--- config.py
+++ config.py
@@ -6,21 +6,16 @@ exclude =  no = False
 # SCGI address or unix socket file path found in your rtorrent.rc file
 scgi = '127.0.0.1:5000'
 
-# Unix domain socket - Automated creation by script
+# Unix domain socket - This program will automatically create this file
 socket_file  = 'unix_socket'
 
 # The wait time between cache creation iterations in seconds
 cache_interval = 300
 
-# Receive a notification when disk is full?
-enable_email = no
-enable_pushbullet = no
-enable_telegram = no
-enable_slack = no
 
-###### DISK CHECK SECTION ######
+###### DISK CHECKER SETTINGS ######
 
-# This script will auto detect mount points and only delete torrents inside the moint point that the torrent will be downloaded to
+# This program will auto detect mount points and only delete torrents inside the mount point that the torrent will be downloaded to
 
 # The minimum amount of free space (in Gigabytes) to maintain
 minimum_space = 5
@@ -31,28 +26,27 @@ minimum_space_mp = {
 #                          '/torrents' : 100,
                    }
 
-# GENERAL RULES START
+### GENERAL RULES START ###
 
 # All minimum requirements must be met by a torrent to be deleted
 
 # Torrent Size in Gigabytes / Age in Days
-
 minimum_size = 5
 minimum_age = 7
 minimum_ratio = 1.2
 
-# Only the age of a torrent must be higher or equal to this number to be deleted (torrent size requirement remains) - no to disable
+# Only the age of a torrent must be higher or equal to this number for it to be deleted (torrent size requirement remains) - no to disable
 fallback_age = no
 
-# Only the ratio of a torrent must be higher or equal to this number to be deleted (torrent size requirement remains) - no to disable
+# Only the ratio of a torrent must be higher or equal to this number for it to be deleted (torrent size requirement remains) - no to disable
 fallback_ratio = 1.1
 
-# GENERAL RULES END
+### GENERAL RULES END ###
 
 
 # Tracker Rules will override general rules - Fill to enable
 
-# include: use general rules | exclude: exclude tracker
+# include = use general rules | exclude = exclude tracker
 
 # Value Order: 1. Minimum Torrent Size (GB) 2. Minimum Age 3. Minimum Ratio 4. Fallback Age 5. Fallback Ratio
 
@@ -71,7 +65,7 @@ trackers_only = yes
 
 # Label Rules will override general/tracker rules - Fill to enable
 
-# include: use general/tracker rules | exclude: exclude label
+# include = use tracker rules (if defined) otherwise use general rules | exclude = exclude label
 
 # Value Order: 1. Minimum Torrent Size (GB) 2. Minimum Age 3. Minimum Ratio 4. Fallback Age 5. Fallback Ratio
 
@@ -89,6 +83,12 @@ exclude_unlabelled = no
 
 
 ###### NOTIFICATION SETTINGS ######
+
+# Receive a notification when disk is full?
+enable_email = no
+enable_pushbullet = no
+enable_telegram = no
+enable_slack = no
 
 # Amount of minutes to wait before sending a notification between torrent downloads
 interval = 60
