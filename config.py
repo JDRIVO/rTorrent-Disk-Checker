7dded7b4c3bbc12ed21460ee09ec6939e57eb81b
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-08-10 00:28:41 +1000

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ scgi = '127.0.0.1:5000'
 # Unix domain socket - This program will automatically create this file
 socket_file = 'unix_socket'
 
-# The wait time between cache creation iterations in seconds
+# The amount of seconds to wait before updating the cache
 cache_interval = 300
 
 
