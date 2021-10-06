a69bb558c2cbe9098b2f5a72a70460ea00a15c1b
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-10-06 11:14:14 +1100

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,6 @@ try:
 	message = "|:|".join(sys.argv).encode("utf-8")
 	s.send(message)
 except Exception as e:
-	import os
 	import logging
 	logging.basicConfig(filename="checker_client.log", level=logging.DEBUG, format="%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
 	logging.critical("Server Error: Couldn't process torrent: {}: {}".format(sys.argv[1], e))
