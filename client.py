7cd5e693004d83e9b463c7db8e863a4886c0a1df
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2022-12-06 19:28:33 +1100

    Adjustment to previous fix

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
+import os
 import sys
 import socket
 
+os.chdir(os.path.dirname(sys.argv[0]))
+
 try:
 	import config as cfg
+
 	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 	s.connect(cfg.socket_file)
 	data = "|:|".join(sys.argv).encode("utf-8")
 	s.send(data)
 except Exception as e:
-	import os
 	import logging
 
-	os.chdir(os.path.dirname(sys.argv[0]))
 	logging.basicConfig(filename="diskchecker.log", level=logging.DEBUG, format="%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
 	logging.critical("client.py: Couldn't process torrent: {}: {}".format(sys.argv[1], e))
